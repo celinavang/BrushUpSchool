9895a0f2cd3c08c3b0b0ee27ac74076e8a9e1759
Celina Vang Henningsen <civah@hotmail.dk>
2024-02-05 19:28:06 +0100

    Create PersonTests.cs

---
+++ BrushUpSchoolTests/PersonTests.cs
@@ -0,0 +1,32 @@
+﻿using BrushUpSchool;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrushUpSchoolTests
+{
+    [TestClass()]
+    internal class PersonTests
+    {
+        private Person person = new Person { Id = 1, Name = "John" };
+        private Person personNoName = new Person { Id = 2 };
+        private Person personShortName = new Person { Id = 3, Name = ""};
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            person.ValidateName();
+            Console.WriteLine(person.Name);
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            person.Validate();
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
